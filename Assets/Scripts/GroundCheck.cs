50b6608edb13cd916054554e6c0a64b1decea90d
michaelamatucci <michaelamatucci17@gmail.com>
2020-02-03 00:33:31 -0500

    Initial Project
    (with changed FPS controller)

---
+++ Assets/Scripts/GroundCheck.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundCheck : MonoBehaviour
+{
+    public bool isGround;
+    [SerializeField]
+    private string groundTag = "Ground";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isGround = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == groundTag)
+        {
+            isGround = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.tag == groundTag)
+        {
+            isGround = false;
+        }
+    }
+}
