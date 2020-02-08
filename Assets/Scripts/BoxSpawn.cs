045f75aff0da49b5ed8bfd15089f33ba26b68775
michaelamatucci <michaelamatucci17@gmail.com>
2020-02-07 23:21:16 -0500

    Health/Ammo Systems
    Added PlayerManager Script to handle health and ammo systems
    Only one type of ammo (will be changed later with more weapons)
    Health/Ammo pickups + spawn points (if pickup respawning is required, most likey won't be)

---
+++ Assets/Scripts/BoxSpawn.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxSpawn : MonoBehaviour
+{
+    public float waitTime;
+    public GameObject boxType;
+    private GameObject currentBox;
+    private bool respawning = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnBox();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(currentBox == null && respawning == false)
+        {
+            StartCoroutine(RespawnBox());
+        }
+    }
+
+    void SpawnBox()
+    {
+        currentBox = Instantiate(boxType, this.transform);
+    }
+
+    IEnumerator RespawnBox()
+    {
+        respawning = true;
+        yield return new WaitForSeconds(waitTime);
+        SpawnBox();
+        respawning = false;
+    }
+}
---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    #region public variables
+    public int health = 100;
+    public int ammo = 100;
+    #endregion
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
