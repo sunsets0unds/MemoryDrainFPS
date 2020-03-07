8c345ba2b65cb6eedf44738347d76a6b1189622f
michaelamatucci <michaelamatucci17@gmail.com>
2020-03-07 13:21:56 -0500

    Added Weapon Behavior
    Added projectile weapon
    Updated Player script

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -6,19 +6,23 @@ public class PlayerManager : MonoBehaviour
 {
     #region public variables
     public int health = 100;
+    public int maxHealth = 100;
     public int ammo = 100;
+    public int maxAmmo = 150;
+    [HideInInspector]
+    public bool haveAmmo;
     #endregion
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (health > maxHealth)
+            health = maxHealth;
+        if (ammo > maxAmmo)
+            ammo = maxAmmo;
+        if (ammo > 0)
+            haveAmmo = true;
+        else
+            haveAmmo = false;
     }
 }
---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float lifespan = 5f;
+    public int damage = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(projectileLifespanCo());
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag != this.tag)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag != this.tag)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    private IEnumerator projectileLifespanCo()
+    {
+        yield return new WaitForSeconds(lifespan);
+
+        Destroy(this.gameObject);
+    }
+}
